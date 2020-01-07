0fed8d19acad4dd71f078ffc6189f9a5ac1473d2
Rodrigo Fernandez <rocketrodrigo@gmail.com>
2020-01-07 21:13:07 +0000

    Nuevos formatos

--- report/__init__.py
+++ report/__init__.py
@@ -1,5 +1,6 @@
 # -*- encoding: utf-8 -*-
 
 import report_voucher
+import report_invoice
 # import report_saleorder_domex
 # import report_saleordershipping_domex
--- report/report_voucher.py
+++ report/report_voucher.py
@@ -8,6 +8,27 @@ class ReportAbstractVoucher(models.AbstractModel):
 
     nombre_reporte = ''
 
+    def anio(self, o):
+        partes = o.payment_date.split('-')
+        if len(partes) > 0:
+            return partes[0]
+        else:
+            return ''
+
+    def mes(self, o):
+        partes = o.payment_date.split('-')
+        if len(partes) > 1:
+            return partes[1]
+        else:
+            return ''
+
+    def dia(self, o):
+        partes = o.payment_date.split('-')
+        if len(partes) > 2:
+            return partes[2]
+        else:
+            return ''
+
     def totales(self, o):
         t = {'debito': 0, 'credito': 0}
         for l in o.move_line_ids:
@@ -26,6 +47,9 @@ class ReportAbstractVoucher(models.AbstractModel):
             'docs': docs,
             'a_letras': odoo.addons.l10n_gt_extra.a_letras,
             'totales': self.totales,
+            'anio': self.anio,
+            'dia': self.dia,
+            'mes': self.mes,
         }
         return self.env['report'].render(self.nombre_reporte, docargs)
 
@@ -76,3 +100,87 @@ class ReportVoucher8(models.AbstractModel):
     _inherit = 'domex.abstract.reporte_voucher'
 
     nombre_reporte = 'domex.reporte_voucher8'
+
+class ReportVoucher9(models.AbstractModel):
+    _name = 'report.domex.reporte_voucher9'
+    _inherit = 'domex.abstract.reporte_voucher'
+
+    nombre_reporte = 'domex.reporte_voucher9'
+
+class ReportVoucher10(models.AbstractModel):
+    _name = 'report.domex.reporte_voucher10'
+    _inherit = 'domex.abstract.reporte_voucher'
+
+    nombre_reporte = 'domex.reporte_voucher10'
+
+class ReportVoucher11(models.AbstractModel):
+    _name = 'report.domex.reporte_voucher11'
+    _inherit = 'domex.abstract.reporte_voucher'
+
+    nombre_reporte = 'domex.reporte_voucher11'
+
+class ReportVoucher12(models.AbstractModel):
+    _name = 'report.domex.reporte_voucher12'
+    _inherit = 'domex.abstract.reporte_voucher'
+
+    nombre_reporte = 'domex.reporte_voucher12'
+
+class ReportVoucher13(models.AbstractModel):
+    _name = 'report.domex.reporte_voucher13'
+    _inherit = 'domex.abstract.reporte_voucher'
+
+    nombre_reporte = 'domex.reporte_voucher13'
+
+class ReportVoucher14(models.AbstractModel):
+    _name = 'report.domex.reporte_voucher14'
+    _inherit = 'domex.abstract.reporte_voucher'
+
+    nombre_reporte = 'domex.reporte_voucher14'
+
+class ReportVoucher15(models.AbstractModel):
+    _name = 'report.domex.reporte_voucher15'
+    _inherit = 'domex.abstract.reporte_voucher'
+
+    nombre_reporte = 'domex.reporte_voucher15'
+
+class ReportVoucher16(models.AbstractModel):
+    _name = 'report.domex.reporte_voucher16'
+    _inherit = 'domex.abstract.reporte_voucher'
+
+    nombre_reporte = 'domex.reporte_voucher16'
+
+class ReportVoucher17(models.AbstractModel):
+    _name = 'report.domex.reporte_voucher17'
+    _inherit = 'domex.abstract.reporte_voucher'
+
+    nombre_reporte = 'domex.reporte_voucher17'
+
+class ReportVoucher18(models.AbstractModel):
+    _name = 'report.domex.reporte_voucher18'
+    _inherit = 'domex.abstract.reporte_voucher'
+
+    nombre_reporte = 'domex.reporte_voucher18'
+
+class ReportVoucher19(models.AbstractModel):
+    _name = 'report.domex.reporte_voucher19'
+    _inherit = 'domex.abstract.reporte_voucher'
+
+    nombre_reporte = 'domex.reporte_voucher19'
+
+class ReportVoucher20(models.AbstractModel):
+    _name = 'report.domex.reporte_voucher20'
+    _inherit = 'domex.abstract.reporte_voucher'
+
+    nombre_reporte = 'domex.reporte_voucher20'
+
+class ReportVoucher21(models.AbstractModel):
+    _name = 'report.domex.reporte_voucher21'
+    _inherit = 'domex.abstract.reporte_voucher'
+
+    nombre_reporte = 'domex.reporte_voucher21'
+
+class ReportVoucher22(models.AbstractModel):
+    _name = 'report.domex.reporte_voucher22'
+    _inherit = 'domex.abstract.reporte_voucher'
+
+    nombre_reporte = 'domex.reporte_voucher22'
