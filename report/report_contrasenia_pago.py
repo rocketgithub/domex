d8f1ce29d495e0d5dd7514e39e97a02032098a8e
jefferson <jeffer.leo93@gmail.com>
2020-06-11 21:49:14 -0600

    Quitar logging

--- report/report_contrasenia_pago.py
+++ report/report_contrasenia_pago.py
@@ -15,7 +15,6 @@ class ReportContraseniaPago(models.AbstractModel):
         return fecha
 
     def _get_facturas(self,facturas):
-        logging.warn(facturas)
         factura_dic = {}
         for factura in facturas:
             if factura.partner_id.id not in factura_dic:
@@ -23,14 +22,9 @@ class ReportContraseniaPago(models.AbstractModel):
             factura_dic[factura.partner_id.id]['total'] += factura.amount_total
             factura_dic[factura.partner_id.id]['compras'].append(factura.origin)
             factura_dic[factura.partner_id.id]['facturas'].append(factura)
-                # factura_dic[factura.id]['compra'] = factura.origin
-                # factura_dic[factura.id]['proveedor'] = factura.partner_id.name
-        logging.warn(factura_dic)
         primer_proveedor = next(iter(factura_dic))
-        # logging.warn(primer_proveedor)
         factura_dic = factura_dic[primer_proveedor]
         factura_dic['compras'] = ','.join(factura_dic['compras'])
-        logging.warn(factura_dic)
         return factura_dic
 
     @api.model
