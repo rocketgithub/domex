b77fcb6c4627d47c2fc5a8fcf7e8cb01eda86cfd
arianacarrillo <arianacarrillo24@gmail.com>
2020-11-13 01:45:13 -0600

    Libro mayor de compras, solucion de tickets y cambios varios.

--- report/__init__.py
+++ report/__init__.py
@@ -5,5 +5,6 @@ import report_invoice
 import report_contrasenia_pago
 import report_purchase_order
 import report_cuentas_pagar_cobrar
+import report_libro_mayor_compras
 # import report_saleorder_domex
 # import report_saleordershipping_domex
--- wizard/__init__.py
+++ wizard/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import contrapartida_anticipo, contrasenia_pago, purchase_order, cuentas_pagar_cobrar
+from . import contrapartida_anticipo, contrasenia_pago, purchase_order, cuentas_pagar_cobrar, libro_mayor_compras
