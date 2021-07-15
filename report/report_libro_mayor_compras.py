a58655845586d41ba0559b3226893c80e2e8ecd3
arianaa24 <arianacarrillo24@gmail.com>
2021-07-15 03:01:04 -0600

    Cambios en Reportes

--- report/report_libro_mayor_compras.py
+++ report/report_libro_mayor_compras.py
@@ -5,76 +5,73 @@ import time
 from datetime import date
 from odoo import api, models, _
 from odoo.exceptions import UserError
-from odoo.tools import DEFAULT_SERVER_DATE_FORMAT
 import logging
-# import odoo.addons.l10n_gt_extra.a_letras
 
 class LibroMayorCompras(models.AbstractModel):
     _name = 'report.domex.libro_mayor_compras'
     
-    def _get_data(self, accounts, date_from, date_to, partner_id):
-        moves_lines = dict(map(lambda x: (x, []), accounts.ids))
-        
-        for account in moves_lines:
-            tipo = {}
-            tipo['local'] = []
-            tipo['exterior'] = []
-            moves_lines[account].append(tipo)
-
-        domain = []
-        domain.append(('account_id.id', 'in', accounts.ids))
+    def _get_data(self, local_accounts, exterior_accounts, date_from, date_to, partner_id):
+        move_lines = {}
+        move_lines['local'] = dict(map(lambda x: (x, []), local_accounts.ids))
+        move_lines['exterior'] = dict(map(lambda x: (x, []), exterior_accounts.ids))
+        domain_l = [('account_id.id', 'in', local_accounts.ids)]
+        domain_e = [('account_id.id', 'in', exterior_accounts.ids)]
         if date_from:
-            domain.append(('date', '>=', date_from))
+            domain_l.append(('date', '>=', date_from))
+            domain_e.append(('date', '>=', date_from))
         if date_to:
-            domain.append(('date', '<=', date_to))
+            domain_l.append(('date', '<=', date_to))
+            domain_e.append(('date', '<=', date_to))
         if partner_id:
-            domain.append(('partner_id.id', '<=', partner_id))
-        MoveLines = self.env['account.move.line'].search(domain) 
-        for row in MoveLines:
+            domain_l.append(('partner_id.id', '<=', partner_id))
+            domain_e.append(('partner_id.id', '<=', partner_id))
+        MoveLines_locales = self.env['account.move.line'].search(domain_l)
+        MoveLines_exterior = self.env['account.move.line'].search(domain_e)
+        
+        for row in MoveLines_locales:
             balance = 0
-            invoice = ""
-            tipo_gasto = ""
-            if row.invoice_id:
-                invoice = row.invoice_id.name
-                tipo_gasto = row.invoice_id.tipo_gasto
-                
             ln = {}
-            ln = {'lid': row.id, 'lname': row.name, 'partner_name': row.partner_id.name, 'debit': row.debit, 'move_name': row.move_id.name, 'currency_id': row.currency_id, 'credit': row.credit, 'lref': row.ref, 'amount_currency': row.amount_currency, 'balance': row.debit - row.credit, 'total_balance': 0, 'ldate': row.date, 'date_due': row.date_maturity, 'tipo_gasto': tipo_gasto}
-                   
-            if tipo_gasto == 'importacion':
-                moves_lines[row.account_id.id][0]['exterior'].append(ln)
-            else:
-                moves_lines[row.account_id.id][0]['local'].append(ln)
-            
-            
-            for line in moves_lines[row.account_id.id][0]['exterior']:
+            ln = {'lid': row.id, 'lname': row.name, 'partner_name': row.partner_id.name, 'debit': row.debit, 'move_name': row.move_id.name, 'currency_id': row.currency_id, 'credit': row.credit, 'lref': row.ref, 'amount_currency': row.amount_currency, 'balance': row.debit - row.credit, 'total_balance': 0, 'ldate': row.date, 'date_due': row.date_maturity}
+            move_lines['local'][row.account_id.id].append(ln)
+            for line in move_lines['local'][row.account_id.id]:
                 balance += line['balance']
                 line['total_balance'] += balance
-                
-            for line in moves_lines[row.account_id.id][0]['local']:
+                    
+        for row in MoveLines_exterior:
+            balance = 0
+            ln = {}
+            ln = {'lid': row.id, 'lname': row.name, 'partner_name': row.partner_id.name, 'debit': row.debit, 'move_name': row.move_id.name, 'currency_id': row.currency_id, 'credit': row.credit, 'lref': row.ref, 'amount_currency': row.amount_currency, 'balance': row.debit - row.credit, 'total_balance': 0, 'ldate': row.date, 'date_due': row.date_maturity}
+            move_lines['exterior'][row.account_id.id].append(ln)
+            for line in move_lines['exterior'][row.account_id.id]:
                 balance += line['balance']
                 line['total_balance'] += balance
-                
-            
-        account_res = []
-        for account in accounts:
-            res = dict((fn, 0.0) for fn in ['credit', 'debit', 'balance', 'local_credit', 'local_debit', 'local_balance', 'exterior_credit', 'exterior_debit', 'exterior_balance'])
+                    
+        account_res = [[], []]
+        for account in local_accounts:
+            res = dict((fn, 0.0) for fn in ['credit', 'debit', 'balance'])
             res['code'] = account.code
             res['name'] = account.name
-            res['local_move_lines'] = moves_lines[account.id][0]['local']
-            res['exterior_move_lines'] = moves_lines[account.id][0]['exterior']
+            res['move_lines'] = move_lines['local'][account.id]
             res['currency_id'] = account.currency_id
-            for line in res.get('local_move_lines'):
-                res['local_debit'] += line['debit']
-                res['local_credit'] += line['credit']
-                res['local_balance'] = line['total_balance']
-            for line in res.get('exterior_move_lines'):
-                res['exterior_debit'] += line['debit']
-                res['exterior_credit'] += line['credit']
-                res['exterior_balance'] = line['total_balance']
-            account_res.append(res)
+            for line in res.get('move_lines'):
+                res['debit'] += line['debit']
+                res['credit'] += line['credit']
+                res['balance'] = line['total_balance']
+            account_res[0].append(res)
+        for account in exterior_accounts:
+            res = dict((fn, 0.0) for fn in ['credit', 'debit', 'balance'])
+            res['code'] = account.code
+            res['name'] = account.name
+            res['move_lines'] = move_lines['exterior'][account.id]
+            res['currency_id'] = account.currency_id
+            for line in res.get('move_lines'):
+                res['debit'] += line['debit']
+                res['credit'] += line['credit']
+                res['balance'] = line['total_balance']
+            account_res[1].append(res)
         return account_res
-   
+        
+        
     @api.model
     def render_html(self, docids, data=None):
         if not data.get('form') or not self.env.context.get('active_model'):
@@ -84,8 +81,9 @@ class LibroMayorCompras(models.AbstractModel):
         date_from = data['form']['date_from']
         date_to = data['form']['date_to']
         partner_id = data['form']['partner_id']
-        accounts = self.env['account.account'].search([('id', 'in', data['form']['account_ids'])])
-        accounts_res = self._get_data(accounts, date_from, date_to, partner_id)
+        local_accounts = self.env['account.account'].search([('id', 'in', data['form']['local_account_ids'])])
+        exterior_accounts = self.env['account.account'].search([('id', 'in', data['form']['exterior_account_ids'])])
+        accounts_res = self._get_data(local_accounts, exterior_accounts, date_from, date_to, partner_id)
         docargs = {
             'doc_ids': docids,
             'doc_model': self.model,
--- wizard/libro_mayor_compras.py
+++ wizard/libro_mayor_compras.py
@@ -7,13 +7,13 @@ class WizardLibroMayorCompras(models.TransientModel):
     _inherit = "account.common.account.report"
     _name = "domex.wizard_libro_mayor_compras"
 
-    account_ids = fields.Many2many("account.account", string="Cuenta", required=True, default=False)
+    local_account_ids = fields.Many2many("account.account",'model_local_rel', string="Cuenta para Compras Locales", required=True, default=False)
+    exterior_account_ids = fields.Many2many("account.account", 'model_exterior_rel', string="Cuenta para Compras Exterior", required=True, default=False)
     compania_id = fields.Many2one('res.company', string='Compañía', required=True)
     partner_id = fields.Many2one('res.partner', string='Partner')
-    
 
     def _print_report(self, data):
         data = self.pre_print_report(data)
-        data['form'].update({'compania_id': self.compania_id.name, 'account_ids': self.account_ids.ids, 'partner_id': self.partner_id.id})
+        data['form'].update({'compania_id': self.compania_id.name, 'local_account_ids': self.local_account_ids.ids, 'exterior_account_ids': self.exterior_account_ids.ids, 'partner_id': self.partner_id.id})
         records = self.env[data['model']].browse(data.get('ids', []))
         return self.env['report'].with_context(landscape=True).get_action(records, 'domex.libro_mayor_compras', data=data) 
